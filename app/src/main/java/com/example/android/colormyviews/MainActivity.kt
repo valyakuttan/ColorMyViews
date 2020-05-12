16d7f430bec9b72222cf2ab6c9c5984fe247b08e
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-12 20:32:59 +0530

    add a horizontal chain of buttons

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-            box_four_text, box_five_text, constraint_layout)
+            box_four_text, box_five_text, constraint_layout,
+            red_button, yellow_button, green_button)
 
         for (item in clickableViews) {
             item.setOnClickListener{
@@ -43,6 +44,15 @@ class MainActivity : AppCompatActivity() {
             R.id.box_five_text -> view.setBackgroundResource(
                 android.R.color.holo_green_light
             )
+
+            // boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(
+                R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(
+                R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(
+                R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
 
